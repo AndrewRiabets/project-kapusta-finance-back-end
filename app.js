4d15d3e48924ad04f7f2444685a0d66f4d9a963f
Kapral-Yrii <kapral2701@gmail.com>
2022-02-13 23:30:38 +0200

    enable support cookies

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ import guard from './middleware/guard.middleware'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}))
 app.use(express.json())
 app.use(cookieParser())
 
